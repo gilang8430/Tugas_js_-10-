16a0207d5f9ead174611dc06f1ec460d965049ea
gilang_gans <gilangsos99@gmail.com>
2022-05-15 15:54:03 +0700

    Tugas js 10
    Tugas ini untuk niomic

---
+++ javascript/Tugas10.js
@@ -0,0 +1,9 @@
+function panggilPerulangan(){
+    var arr = ["1","3","6","10","15","21","28","36","45","55"]
+    for (var i = 0; i < arr.length; i++)
+    console.log(arr[i]);
+  }
+
+
+
+panggilPerulangan();
